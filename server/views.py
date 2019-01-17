c211bd297aedbd327339c5e6eea05aeb2a30792a
mamamadoge <mamamadoge@gmail.com>
2019-01-17 11:59:28 +0800

    v1.3

--- server/views.py
+++ server/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.template import RequestContext
 from server import models
+from AIClass import settings
 # Create your views here.
 
 
@@ -9,7 +10,7 @@ def index(request):
 
 
 def student_list(request):
-    user_list = []
+    stu_list = []
     if request.method == 'POST':
         id = request.POST.get('stu_id')
         name = request.POST.get('stu_name')
@@ -17,8 +18,8 @@ def student_list(request):
         age = request.POST.get('stu_age')
         print(id, name)
         models.Student.objects.create(stu_id=id, stu_name=name, stu_sex=sex, stu_age=age)
-    user_list = models.Student.objects.all()
-    return render(request, 'server_temp/student_tables.html', {'data': user_list})
+    stu_list = models.Student.objects.all()
+    return render(request, 'server_temp/student_tables.html', {'data': stu_list})
 
 
 def teacher_list(request):
@@ -44,6 +45,43 @@ def course_list(request):
     course_list = models.Course.objects.all()  # 查询教师信息
     return render(request, 'server_temp/course_tables.html', {'data': course_list})
 
+
+def student_detail(request):
+    if request.method == "GET":
+        id = request.GET.get('stu_id')
+        student = models.Student.objects.get(stu_id=id)
+        if student:
+            return render(request, 'server_temp/student_detail.html', {'data': student})
+
+
+def student_edit(request):
+    if request.method == "POST":
+        id = request.POST.get('stu_id')
+        name = request.POST.get('stu_name')
+        # photo = request.POST.get('stu_photo')
+        try:
+            photo = request.FILES['stu_photo']
+            fname = '%s/img/%s' % (settings.MEDIA_ROOT, photo.name)
+            with open(fname, 'wb') as pic:
+                for c in photo.chunks():
+                    pic.write(c)
+            student = models.Student.objects.get(stu_id=id)
+            student.stu_img = fname
+            student.save()
+        except Exception as e:
+            pass
+        sex = request.POST.get('stu_sex')
+        age = request.POST.get('stu_age')
+        try:
+            student = models.Student.objects.get(stu_id=id)
+            student.stu_name = name
+            student.stu_sex = sex
+            student.stu_age = age
+            student.save()
+            stu_list = models.Student.objects.all()
+            return render(request, 'server_temp/student_tables.html', {'data': stu_list})
+        except Exception as e:
+            return render(request, 'server_temp/error404.html')
 
 
 
